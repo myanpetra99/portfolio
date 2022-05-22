677008360903fef0c80f0de1543599943abf637f
myanpetra99 <myanpetra99@gmail.com>
2022-05-22 15:30:48 +0700

    deploy #5 add email and wa, change picture

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import logo from '../user.jpg'
+import logo from '../user.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import {
   faGithub,
   faLinkedin,
-  faInstagram
+  faInstagram,
+  faWhatsapp
 } from '@fortawesome/free-brands-svg-icons'
 export default class Introduction extends Component {
   render () {
@@ -15,8 +17,11 @@ export default class Introduction extends Component {
           <div className='row'>
             <div className='column'>
               <div className='container-logo'>
+              <div className='container-logo2'>
                 <img src={logo} className='App-logo'></img>
+                </div>
               </div>
+              
 
               <p >
                 <code>&lt; h1 &gt;</code>Hello There  <div className='wave'>👋</div>, I'm Petra<code>&lt;/ h1 &gt;</code>
@@ -37,6 +42,8 @@ export default class Introduction extends Component {
                 </a>
                 <a href='www.linkedin.com/in/michaelyanpetra'  target={'_blank'}><FontAwesomeIcon icon={faLinkedin} className='icon' /></a>
                 <a href='http://instagram.com/myanpetra99s' target={'_blank'}><FontAwesomeIcon icon={faInstagram} className='icon' /></a>
+                <a href='mailto:myanpetra99@gmail.com'><FontAwesomeIcon icon={faEnvelope} className='icon' /></a>
+                <a href='https://wa.me/087788180748' target={'_blank'}><FontAwesomeIcon icon={faWhatsapp} className='icon' /></a>
                 <code>&lt;/ div &gt;</code>
               </div>
             </div>
